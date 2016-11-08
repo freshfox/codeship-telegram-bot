bf7c33c679a8c6f459f9341dbb160dcd3ba9a916
Alexander Ott <contact@alexanderott.at>
2016-11-08 15:20:09 +0100

    Update format of welcome message and notification

--- api/services/formatter-service.js
+++ api/services/formatter-service.js
@@ -10,8 +10,15 @@ class FormatterService {
 
 	getStartMessage(chatId) {
 		let hook = this.getWebHook(chatId);
-		return `${FormatterService.EMOJI.ship} Add this URL to your Codeship notification settings:
+		return `${FormatterService.EMOJI.ship} Hi! I see you want to receive Codeship notifications.
+				Just add this URL as a Webhook to your Codeship notification settings to receive notifications in this conversation.
+				
+				To receive <b>only failing builds</b> (and the recovering builds)
 				${hook}
+
+				To receive <b>all builds</b> (succeeding and failing)
+				${hook}?mode=all
+
 				@codeship_bot by @dominic0`;
 	}
 
@@ -28,8 +35,8 @@ FormatterService.EMOJI = {
 };
 
 function defaultFormat(build) {
-	return `${FormatterService.EMOJI.ship} <b>${build.project_name}:</b> ${build.message}
-			on <code>${build.branch}</code> - <b>${build.status}</b> ${FormatterService.EMOJI[build.status] || ''}
+	return `${FormatterService.EMOJI.ship} <b>${build.project_name}</b> - <code>${build.branch}</code> ${FormatterService.EMOJI[build.status] || ''}
+ 			<b>${build.committer}</b>: ${build.message}
 			<a href="${build.build_url}">Open on Codeship</a>`;
 }
 
