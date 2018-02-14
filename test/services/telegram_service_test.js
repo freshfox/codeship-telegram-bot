ad145494d5e69380e98ac6e2ab9ae8257a1749af
Dominic Bartl <dominic.bartl.at@gmail.com>
2018-02-14 17:18:41 +0100

    Skip telegram service test

--- test/services/telegram_service_test.js
+++ test/services/telegram_service_test.js
@@ -3,7 +3,7 @@ const service = require('../../api/services/telegram-service');
 
 describe('TelegramService', function () {
 
-    it('should send a message', () => {
+    it.skip('should send a message', () => {
 
     	return service.send(-147834436, 'Hello')
 
